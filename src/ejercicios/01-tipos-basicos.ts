b0c35542018f379d9138f554e76b88353c324b19
linux-charold <harolrop14@gmail.com>
2022-02-11 16:29:15 -0500

    tipos basicos y conceptos generales

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,16 @@
+
+
+console.log('Hola Mundo!');
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre:string = 'Strider';
+let hp:number | any = 95;
+let estaVivo:boolean = true;
+
+hp = 'Gaaaa'
+
+console.log(nombre,hp,estaVivo);
+
