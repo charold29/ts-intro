1629fc28b05545a7830b67c8ee608588cc7a14b0
linux-charold <harolrop14@gmail.com>
2022-02-12 15:46:46 -0500

    Desestructuracion de arreglos

---
+++ src/ejercicios/07-deses-arreglos.ts
@@ -0,0 +1,11 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+const naruto: string[] = ['Naruto', 'Sasuke', 'Sakura'];
+
+const [,,psj3] = naruto;
+
+// console.log('Personaje 1: ', psj1);
+// console.log('Personaje 2: ', psj2);
+console.log('Personaje 3: ', psj3);
