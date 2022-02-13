b0a707174b522dd5827e812ad46e1bfe48a6450e
linux-charold <harolrop14@gmail.com>
2022-02-13 16:41:35 -0500

    encadenamiento opcional " ? || "

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,3 @@
 /*
     ===== Código de TypeScript =====
 */
-
-interface Pasajero {
-    nombre:string;
-    hijos?:string[];
-}
-
-const pasajero1:Pasajero = {
-    nombre: 'Harold'
-}
-
-const pasajero2:Pasajero = {
-    nombre: 'Melisa',
-    hijos: ['Mateo', 'Luis']
-}
-
-function imprimeHijos(pasajero:Pasajero):void{
-   
-    const numHijos = pasajero.hijos.length;
-    
-    console.log(numHijos);
-}
-
-imprimeHijos(pasajero2);
